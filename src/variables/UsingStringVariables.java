cd78d42cea83768a66a930342d976dd91940116b
vas-chif <vasile.chifeac@gmail.com>
2021-09-11 16:50:03 +0200

    Added Variable String example

---
+++ src/variables/UsingStringVariables.java
@@ -0,0 +1,11 @@
+package src.variables;
+
+public class UsingStringVariables {
+    public static void main(String[] args) {
+        String firstName = "Elina";
+        String lastName = "Ch";
+
+        System.out.println("Last name: " + lastName);
+        System.out.println("First name: " + firstName);
+    }
+}
