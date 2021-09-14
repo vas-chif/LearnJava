ce26b594ea4f79ffb7b1c8c47d8fbb14838f3556
vas-chif <vasile.chifeac@gmail.com>
2021-09-14 10:47:49 +0200

    Added file with determined the type of triangle

---
+++ src/folowControl/typetriangle/DetermineTypeTriangle.java
@@ -0,0 +1,24 @@
+package src.folowControl.typetriangle;
+
+import java.util.Scanner;
+
+//Write a Java program to check whether a triangle is equilateral, scalene or isosceles.
+// Ask the user to provide the length of each side.
+public class DetermineTypeTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the sides of triangles: \n side a: ");
+        int a = sc.nextInt();
+        System.out.println("side b: ");
+        int b = sc.nextInt();
+        System.out.println("side c: ");
+        int c = sc.nextInt();
+
+        if ((a == b) && (a == c)) {
+            System.out.println("This triangle is equilateral.");
+        } else if ((a != b) && (a != c) && (b != c)){
+            System.out.println("This triangle is scalene.");
+        } else
+            System.out.println("This triangle is isosceles.");
+    }
+}
