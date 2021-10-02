87872ff71e718a4d473427ea4fe9345862106640
vas-chif <vasile.chifeac@gmail.com>
2021-10-02 06:00:23 +0200

    Addedexercise with poiiny 2D

---
+++ src/packagesAndImport/points/PintMain.java
@@ -0,0 +1,15 @@
+package src.packagesAndImport.points;
+
+import java.util.Scanner;
+
+public class PintMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Point point = new Point();
+        System.out.print("Please enter the coordinate for x: ");
+        point.x = sc.nextInt();
+        System.out.print("Please enter the coordinate for y: ");
+        point.y = sc.nextInt();
+        point.determineQuadrant();
+    }
+}
---
+++ src/packagesAndImport/points/Point.java
@@ -0,0 +1,24 @@
+package src.packagesAndImport.points;
+
+/*
+Create a class Point with two attributes, x and y, and a method to determine to which quadrant of the
+2D coordinate system this point belongs to. Print the result on the console.
+ */
+public class Point {
+    int x;
+    int y;
+
+    public void determineQuadrant() {
+        if (x > 0 && y > 0) {
+            System.out.println("1° quadrant ");
+        } else if (x < 0 && y > 0) {
+            System.out.println("2° quadrant");
+        } else if (x < 0 && y < 0) {
+            System.out.println("3° quadrant");
+        } else if (x > 0 && y < 0){
+            System.out.println("4° quadrant");
+        }else {
+            System.out.println("The point is positioned on one of the two abscissas");
+        }
+    }
+}
